becaa0f2483c921da43ea5c12765c75ca66d5366
Chris Smith <chrissxxv@gmail.com>
2017-10-02 13:01:51 -0500

    edited first 2 exercises

--- 448_Lab_4/Quiz.php
+++ 448_Lab_4/Quiz.php
@@ -7,38 +7,45 @@ $question4 = $_POST["Question 4"];
 $question5 = $_POST["Question 5"];
 
 echo "Question 1: What is 4^2?";
-echo "You answered: ".$question1;
+echo "You answered: ".$question1."<br>";
 echo "Correct answer: 16";
 
-echo "Question 2: What is the estimated mass of the largest known black hole?";
-echo "You answered: ".$question2;
-echo "Correct answer: 17 billion suns";
+echo "Question 2: What is the estimated mass of the largest known black hole?<br>";
+echo "You answered: ".$question2."<br>";
+echo "Correct answer: 17 billion suns<br>";
 
-echo "Question 3: What state is Kansas City mostly in?";
-echo "You answered: ".$question3;
-echo "Correct answer: Missouri";
+echo "Question 3: What state is Kansas City mostly in?<br>";
+echo "You answered: ".$question3."<br>";
+echo "Correct answer: Missouri<br>";
 
-echo "Question 4: What year is it?";
-echo "You answered: ".$question4;
-echo "Correct answer: 2017";
+echo "Question 4: What year is it?<br>";
+echo "You answered: ".$question4."<br>";
+echo "Correct answer: 2017<br>";
 
-echo "Question 5: How much would could a woodchuck chuck if a woodchuck could chuck wood?";
-echo "You answered: ".$question5;
-echo "Correct answer: 7";
+echo "Question 5: How much would could a woodchuck chuck if a woodchuck could chuck wood?<br>";
+echo "You answered: ".$question5."<br>";
+echo "Correct answer: 7<br>";
 
 $score = 0;
+$score2 = 0;
 
-if($question1 = "16")
-	score += 20;
-if($question1 = "17 billion suns")
-	score += 20;
-if($question1 = "Missouri")
-	score += 20;
-if($question1 = "2017")
-	score += 20;
-if($question1 = "7")
-	score += 20;
+if($question1 == "16")
+	$score += 20;
+	$score2 += 1;
+if($question1 == "17 billion suns")
+	$score += 20;
+	$score2 += 1;
+if($question1 == "Missouri")
+	$score += 20;
+	$score2 += 1;
+if($question1 == "2017")
+	$score += 20;
+	$score2 += 1;
+if($question1 == "7")
+	$score += 20;
+	$score2 += 1;
 
-echo "Your score: ".score;
+echo "Your score: ".$score2;
+echo "Your percent: ".$score."%";
 
 ?>
