4ec6f24573476106800821fe25731fcd3581541f
Chris Smith <chrissxxv@gmail.com>
2017-10-02 13:07:15 -0500

    Exercise 1

--- 448_Lab_4/multiplicationTable.php
+++ 448_Lab_4/multiplicationTable.php
@@ -1,17 +1,11 @@
 <?php
 		echo "<table>";
-		$array1 = array();
-		for($i = 0; $i <=100; $i++)
-		{
-			$array1[i]=array();
-		}
-		for($i = 0; $i <=100; $i++)
+		for($i = 1; $i <=100; $i++)
 		{
 			echo "<tr>";
-			for($j = 0; $j <=100; $j++)
+			for($j = 1; $j <=100; $j++)
 			{
-				$array1[i][j] = i*j;
-				echo "<td>".$array1[i][j]."</td>";
+				echo "<td>".$i*$j."</td>";
 			}	
 			echo "</tr>";
 		}
